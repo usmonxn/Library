5d718bfce01b42ed868c92b53b81521578d32eda
usmonxn <usmonxn@users.noreply.github.com>
2026-06-04 18:04:23 +0500

    Remove unrelated uzum project from repository

--- uzumxisobkitob/excelbot/src/main/java/com/example/excelbot/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.excelbot;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
-}
--- uzumxisobkitob/untitled/src/index/html.java
+++
@@ -1,4 +0,0 @@
-package index;
-
-public class html {
-}
